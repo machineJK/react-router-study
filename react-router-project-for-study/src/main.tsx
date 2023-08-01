12e99778cf96111d884e83b8609dda56acc15535
김종건 <wai95@naver.com>
2023-08-01 17:03:40 +0900

    타입스크립트 템플릿으로 변경

--- react-router-project-for-study/src/main.tsx
+++ react-router-project-for-study/src/main.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>test</div>
   }
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
---
+++ react-router-project-for-study/test.ts
@@ -0,0 +1,9 @@
+interface User {
+    name: string,
+    id: number,
+}
+
+const user: User = {
+    name: "John",
+    id: 0,
+};
